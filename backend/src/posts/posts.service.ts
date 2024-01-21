2532a5c1466d80dbd4d7bde795c2bf59ab052cd4
akshatcoder-hash <akshatcoder@outlook.com>
2024-01-22 03:01:34 +0530

    created posts service

--- backend/src/posts/posts.service.ts
+++ backend/src/posts/posts.service.ts
@@ -1,4 +1,27 @@
+// src/posts/posts.service.ts
+
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Post } from './post.entity';
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+  constructor(
+    @InjectRepository(Post)
+    private postsRepository: Repository<Post>,
+  ) {}
+
+  async createPost(
+    title: string,
+    content: string,
+    authorId: string,
+  ): Promise<Post> {
+    const newPost = this.postsRepository.create({ title, content, authorId });
+    return this.postsRepository.save(newPost);
+  }
+
+  async findAll(): Promise<Post[]> {
+    return this.postsRepository.find();
+  }
+}
