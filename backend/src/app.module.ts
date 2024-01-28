0f5cb7d7388b21d0ee2124382eeb17bfa09eb492
akshatcoder-hash <akshatcoder@outlook.com>
2024-01-28 05:34:41 +0530

    fixed typo in post

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,13 +2,17 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PostsModule } from './posts/posts.module'; // Import PostsModule
 import { SupabaseService } from './supabase/supabase.service';
+import { SharedModule } from './shared.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true, // Make ConfigModule global
     }),
+    PostsModule,
+    SharedModule, 
   ],
   controllers: [AppController],
   providers: [AppService, SupabaseService],
